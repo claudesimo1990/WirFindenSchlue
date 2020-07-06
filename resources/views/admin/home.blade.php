4963a9ed17666def2efb71186fbe6e4bd54e9b26
claudesimo1990 <claudesimo1990@gmail.com>
2020-07-06 20:06:01 +0200

    add margin

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<div class="mt-10">
+<div class="mt-11">
     <h1>BienVenue Admin</h1>
     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus distinctio labore officiis soluta veniam. Deserunt dicta doloribus esse eum explicabo, laudantium, modi molestias officia omnis possimus quisquam quo rem veniam.
 </div>
